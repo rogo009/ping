a143513832be89ba78df6d0bc82a98389e65c840
robin <rogo009git@gmail.com>
2020-01-26 19:40:53 -0800

    udpate html and js files

--- script.js
+++ script.js
@@ -4,18 +4,17 @@
 // 	- The `input` field is empty. The message for this error should say *"Whoops! It looks like you forgot to add your email"*
 // 	- The email address is not formatted correctly (i.e. a correct email address should have this structure: `name@host.tld`). The message for this error should say *"Please provide a valid email address"*
 
-const email = document.getElementById('email');
-const submitButton = document.getElementById('submitEmailBtn');
-const errorElement = document.getElementById('errorMessage');
+//Step 1: create variables to target all elements
+const email = document.getElementById('email'); // targets email input
+const submitButton = document.getElementById('submitEmailBtn'); // targets submit button
+const errorElement = document.getElementById('errorMessage'); // target error div
 
 //function
 
-//sounds like i need three functions
 //function one runs each validation method
 //function two runs if input is empty
 //function three runs when input is not empty
 
-//this is not working properly, will NOT validate correctly entered email addresses, with every input the error message appears.
 function validateEmail() {
     let message = [];
     if (email.value === ''); {
@@ -29,5 +28,7 @@ function isEmpty(value) {
     if(value === '') return true;
     return false;
 }
+
+function isEmpty()
 
 submitButton.addEventListener('click', validateEmail);
